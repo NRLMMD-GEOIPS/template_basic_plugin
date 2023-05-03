d9600dcd33433afe39f877690932e8a5f6fbb0e3
mindyls <mindyls@users.noreply.github.com>
2023-05-03 13:20:39 -0700

    Make the commands in test_all.sh functional
    We want test_all.sh to actually run for template_basic_plugin if possible.
    Use actual paths to template_basic_plugin test scripts, with instructions to
    update

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -10,18 +10,23 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-# Please identify and update all instances of "@" found in this script appropriately.
-# You will need to generate one or more test scripts that test your complete functionality,
-# (these scripts provide example geoips calls and sample output, as well as providing a full integration test),
-# and call each script within the test_all.sh script.  Do not rename this script or test directory - automated
-# integrations tests look for the tests/test_all.sh script for complete testing.
+# @ Please identify and update all instances of "@" found in this script appropriately.
+# @ You will need to generate one or more test scripts that test your
+# @ complete functionality, @(these scripts provide example geoips calls and
+# @ sample output, as well as providing a full integration test),
+# @ and call each script within the test_all.sh script.
+
+# Do not rename this script or test directory - automated integration
+# tests look for the tests/test_all.sh script for complete testing.
 
 #!/bin/bash
 
-# This should contain test calls to cover ALL required functionality tests for the @package@ repo.
+# This should contain test calls to cover ALL required functionality tests
+# for the @package@ repo.
 
 # The $GEOIPS tests modules sourced within this script handle:
-   # setting up the appropriate associative arrays for tracking the overall return value,
+   # setting up the appropriate associative arrays for tracking the overall
+   #   return value,
    # calling the test scripts appropriately, and 
    # setting the final return value.
 
@@ -29,13 +34,18 @@
 
 . $GEOIPS/tests/utils/test_all_pre.sh @package@
 
+# @ NOTE: Update "template_basic_plugin" paths below to point to your package's
+# @ test scripts, ie
+# @   $GEOIPS_PACKAGES_DIR/template_basic_plugin -> $GEOIPS_PACKAGES_DIR/@package@
+
 echo ""
 # "call" used in test_all_run.sh
 for call in \
 \
             "$GEOIPS_PACKAGES_DIR/geoips/tests/utils/check_code.sh all `dirname $0`/../" \
-            "$GEOIPS_PACKAGES_DIR/@package@/tests/scripts/test_config.sh" \
-            "$GEOIPS_PACKAGES_DIR/@package@/tests/scripts/@mydatatype@.tc.@product@.imagery_clean.sh"
+            "test_interfaces" \
+            "$GEOIPS_PACKAGES_DIR/template_basic_plugin/tests/scripts/test_config.sh" \
+            "$GEOIPS_PACKAGES_DIR/template_basic_plugin/tests/scripts/@mydatatype@.tc.@product@.imagery_clean.sh"
 do
     . $GEOIPS/tests/utils/test_all_run.sh
 done
