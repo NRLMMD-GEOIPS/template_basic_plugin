9b7049ce1b3f237fc883f6057885422f08f9755e
mindyls <mindyls@users.noreply.github.com>
2023-08-25 08:22:14 -0700

    Updating to correct product names for colormappers tests

--- tests/scripts/clavrx.conus_annotated.cloud-base-python-colors.sh
+++ tests/scripts/clavrx.conus_annotated.cloud-base-python-colors.sh
@@ -16,11 +16,11 @@ run_procflow \
     $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_2023101_1600/clavrx_OR_ABI-L1b-RadF-M6C01_G16_s20231011600207.level2.hdf \
     --procflow single_source \
     --reader_name clavrx_hdf4 \
-    --product_name "My-Cloud-Top-Height" \
+    --product_name "Cloud-Base-Python-Colors" \
     --output_formatter imagery_annotated \
     --filename_formatter geoips_fname \
     --minimum_coverage 0 \
-    --sector_list conus 
+    --sector_list conus
 ss_retval=$?
 
 exit $((ss_retval))
