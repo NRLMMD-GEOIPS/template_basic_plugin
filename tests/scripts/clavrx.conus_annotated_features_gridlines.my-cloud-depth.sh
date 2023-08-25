9f04903cbef8eedd0ac57ad2062e09de1e50402c
mindyls <mindyls@users.noreply.github.com>
2023-08-25 10:22:15 -0700

    Adding prod, alg, annotators, output test scripts
    These are the tutorial test scripts for adding a new product, adding
    a new algorithm, adding new gridline and feature annotators, and using
    a different output formatter.

---
+++ tests/scripts/clavrx.conus_annotated_features_gridlines.my-cloud-depth.sh
@@ -0,0 +1,28 @@
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+#!/bin/bash
+
+run_procflow \
+    $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_2023101_1600/clavrx_OR_ABI-L1b-RadF-M6C01_G16_s20231011600207.level2.hdf \
+    --procflow single_source \
+    --reader_name clavrx_hdf4 \
+    --product_name "My-Cloud-Depth" \
+    --output_formatter imagery_annotated \
+    --filename_formatter geoips_fname \
+    --feature_annotator tutorial \
+    --gridline_annotator tutorial \
+    --minimum_coverage 0 \
+    --sector_list conus 
+ss_retval=$?
+
+exit $((ss_retval))
