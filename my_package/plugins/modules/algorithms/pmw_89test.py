47811d9b9e2d7f82f2b046653fcd72cb206fe955
mindyls <13873624+mindyls@users.noreply.github.com>
2025-04-28 17:23:43 +0000

    Update license blurb directly on main

--- my_package/plugins/modules/algorithms/pmw_89test.py
+++ my_package/plugins/modules/algorithms/pmw_89test.py
@@ -1,4 +1,4 @@
-# # # This source code is protected under the license referenced at
+# # # This source code is subject to the license referenced at
 # # # https://github.com/NRLMMD-GEOIPS.
 
 # @ An algorithm plugin specifies data manipulation steps required for a particualr
